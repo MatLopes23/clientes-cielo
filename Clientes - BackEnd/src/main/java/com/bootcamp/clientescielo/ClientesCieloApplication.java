57dc6007694eb4c7790555eb87c0f600a8140ccf
Mateus Lopes <mateusfellipe2302@gmail.com>
2023-09-25 20:10:37 -0300

    Commit inicial api java

---
+++ Clientes - BackEnd/src/main/java/com/bootcamp/clientescielo/ClientesCieloApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.clientescielo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesCieloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesCieloApplication.class, args);
+	}
+
+}
---
+++ Clientes - BackEnd/src/test/java/com/bootcamp/clientescielo/ClientesCieloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.clientescielo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesCieloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
