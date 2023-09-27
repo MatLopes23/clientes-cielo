d789f32ed51152ce094a155b2a4a55c15e980870
Mateus Lopes <mateusfellipe2302@gmail.com>
2023-09-26 22:11:05 -0300

    Adiciona Cliente Controller

---
+++ Clientes - BackEnd/src/main/java/com/bootcamp/clientescielo/controller/ClienteController.java
@@ -0,0 +1,74 @@
+package com.bootcamp.clientescielo.controller;
+
+import com.bootcamp.clientescielo.model.Cliente;
+import com.bootcamp.clientescielo.service.ClienteService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cliente")
+public class ClienteController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ClienteController.class);
+
+    private final ClienteService clienteService;
+
+    @Autowired
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Cliente>> listarClientes() {
+        logger.info("Lista todos os clientes cadastrados.");
+
+        List<Cliente> clientes = clienteService.listarClientes();
+        return ResponseEntity.ok(clientes);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> buscarClientePorId(@PathVariable Long id) {
+        logger.info("Endpoint: Busca por cliente com ID: {}", id);
+
+        Cliente cliente = clienteService.buscarClienteOuFalha(id);
+        logger.info("Cliente recuperado com sucesso. ID: {}", id);
+
+        return ResponseEntity.ok(cliente);
+    }
+
+    @PostMapping
+    public ResponseEntity<Cliente> criarCliente(@RequestBody Cliente cliente) {
+        logger.info("Endpoint: Criar cliente");
+
+        Cliente novoCliente = clienteService.criarCliente(cliente);
+        logger.info("Cliente criado com sucesso. ID: {}", novoCliente.getId());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(cliente);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> atualizarCliente(@PathVariable Long id, @RequestBody Cliente cliente) {
+        logger.info("Endpoint: Atualizar cliente. ID: {}", id);
+
+        Cliente clienteAtualizado = clienteService.atualizarCliente(id, cliente);
+        logger.info("Cliente atualizado com sucesso. ID: {}", clienteAtualizado.getId());
+
+        return ResponseEntity.ok(clienteAtualizado);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluirCliente(@PathVariable Long id) {
+        logger.info("Endpoint: Excluir cliente. ID: {}", id);
+
+        clienteService.excluirCliente(id);
+        logger.info("Cliente excluido com sucesso. ID: {}", id);
+
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ Clientes - BackEnd/src/main/java/com/bootcamp/clientescielo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.bootcamp.clientescielo.exception;
+
+import com.bootcamp.clientescielo.dto.ErrorResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(ClienteNaoEncontradoException.class)
+    public ResponseEntity<Object> handleClienteNaoEncontradoException(ClienteNaoEncontradoException ex) {
+        logger.warn("Uma exceção ClienteNaoEncontradoException ocorreu: {}", ex.getMessage());
+
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ClienteExistenteException.class)
+    public ResponseEntity<Object> handleClienteExistenteException(ClienteExistenteException ex) {
+        logger.warn("Uma exceção ClienteExistenteException ocorreu. {}", ex.getMessage());
+
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleException(Exception ex) {
+        logger.error("Uma exceção não tratada ocorreu: {}", ex.getMessage());
+
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Ocorreu um erro inesperado.");
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
